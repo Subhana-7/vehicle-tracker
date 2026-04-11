8dca13687f89a6cce196b6819e128bf6b5eee2c5
Subhana <subhanasn2005@gmail.com>
2026-04-11 21:14:56 +0530

    add API to get trip data

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,3 +1,5 @@
+import { analyzeTrip, calculateTrip } from "../service/trip";
+
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
@@ -26,4 +28,19 @@ router.post('/upload', upload.single('file'), (req:any, res:any) => {
       res.json({ message: "Saved to DB", trip });
     });
 });
+
+router.get('/trip/:id', async (req:any, res:any) => {
+  const trip = await Trip.findById(req.params.id);
+
+  const calc = calculateTrip(trip.data);
+  const analysis = analyzeTrip(calc.data);
+
+  res.json({
+    distance: calc.totalDistance,
+    idling: analysis.idlingTime,
+    stoppage: analysis.stoppageTime,
+    data: calc.data
+  });
+});
+
 module.exports = router;
