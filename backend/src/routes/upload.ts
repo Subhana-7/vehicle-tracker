76214c63fe20f8a8627aba729630ed456aa0b195
Subhana <subhanasn2005@gmail.com>
2026-04-11 20:36:55 +0530

    Parse CSV data

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,12 +1,22 @@
 const express = require('express');
 const multer = require('multer');
+const fs = require('fs');
+const csv = require('csv-parser');
 
 const router = express.Router();
 
 const upload = multer({ dest: 'uploads/' });
 
 router.post('/upload', upload.single('file'), (req:any, res:any) => {
-  res.send('File uploaded successfully');
-});
+  const results:any = [];
 
+  fs.createReadStream(req.file.path)
+    .pipe(csv())
+    .on('data', (data:any) => {
+      results.push(data);
+    })
+    .on('end', () => {
+      res.json(results); // temporary
+    });
+});
 module.exports = router;
