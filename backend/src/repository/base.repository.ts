228dd09d170238a0ad03aa6a7d68e0e28766d592
Subhana <subhanasn2005@gmail.com>
2026-04-12 14:37:23 +0530

    add base repository with generic CRUD methods

---
+++ backend/src/repository/base.repository.ts
@@ -0,0 +1,13 @@
+import { Model, Document } from "mongoose";
+
+export class BaseRepository<T extends Document> {
+  constructor(private model: Model<T>) {}
+
+  async create(data: Partial<T>): Promise<T> {
+    return this.model.create(data);
+  }
+
+  async findById(id: string): Promise<T | null> {
+    return this.model.findById(id);
+  }
+}
